00dd1a5ace4cb795ebc0a043ca09f3d8c0e5214f
S-Yucel <113521835+S-Yucel@users.noreply.github.com>
2022-12-28 18:43:30 +0300

    Add files via upload

---
+++ generics/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyList<Customer> cities = new MyList<Customer>();
+        cities.add(new Customer());
+        cities.add(new Customer());
+        cities.add(new Customer());
+
+
+
+    }
+}
